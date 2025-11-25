c8178bc514cfc8f68c41318d257c52d19cc74d99
Arthur Aguiar <arthur.espinaguiar@gmail.com>
2025-11-25 14:10:07 -0300

    New Input System Base Started

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void Move() { }
+
+    void GroundCheck()
+    {
+
+    }
+}
